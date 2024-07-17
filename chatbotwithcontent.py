20dd91731a8a78b895ab1c8251f963976925d699
Ömer Faruk Zeybek <zeybeko22@itu.edu.tr>
2024-07-17 12:51:54 +0300

    Update chatbotwithcontent.py
    Decided to use gemini in this project

--- chatbotwithcontent.py
+++ chatbotwithcontent.py
@@ -2,9 +2,10 @@ import sqlite3
 import requests
 from bs4 import BeautifulSoup
 import json
-import openai
+import requests
+import google.generativeai as genai
 
-api_key = "api_key"
+api_key = "api-key"
 
 #Fetch data from Sqlite
 def fetch_data():
@@ -28,23 +29,3 @@ for row in data:
     articles.append(article)
 
 
-#Can't use api right now because of billing :(
-def train_chatbot(api_key, articles):
-    
-    openai.api_key = api_key
-
-    train_data = []
-    for article in articles:
-        content = article["content"]["p"]
-        train_data.append({"text": content})
-        
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-instruct",
-        messages=train_data,
-        prompt = "You're an AI assistant at the Scholar Law Firm named Bilgin Hukuk. I have taught you the data on this site, use this data to give short and concise answers to people. When they ask for help, tell them to contact you with your contact details. Speak Turkish"
-    )
-
-    reply = response["choices"][0].message.content
-    print(f"Bilgin AI: {reply}")
-
-train_chatbot(api_key, articles)
