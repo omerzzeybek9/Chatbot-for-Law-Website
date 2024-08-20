d85a95d8567ab25a8602aa7304b2f0d6d8c86dc2
Ömer Faruk Zeybek <zeybeko22@itu.edu.tr>
2024-08-20 13:03:42 +0300

    New

--- modeltraining.py
+++ modeltraining.py
@@ -1,4 +1,20 @@
-from openai import OpenAI
+import openai
 
-open_api_key = "api"
+openai.api_key = "api"
+
+response = openai.files.create(
+    file = open("formatted_data.jsonl", "rb"),
+    purpose = "fine-tune"
+)
+
+file_id = response.id
+
+
+fine_tune = openai.fine_tuning.jobs.create(
+  training_file=file_id, 
+  model="gpt-3.5-turbo"
+)
+
+
+
 
